442229ff431579d6fe01ed0e121041a9dc8bc360
LucasRocha <lucasrochinha2007@gmail.com>
2026-09-11 15:35:30 -0300

    Adicionando comentário do ex3

--- Exercício 3/Main3.java
+++ Exercício 3/Main3.java
@@ -1,3 +1,18 @@
+/*
+3. Calculadora interativa
+Desenvolva uma calculadora de console que apresenta repetidamente o seguinte menu:.
+1. soma;
+2. subtração;
+3. multiplicação;
+4. divisão;
+5. potenciação;
+6. resto da divisão;
+7. encerrar.
+Após a seleção de uma operação, o programa deverá solicitar os valores necessários, executar o cálculo e
+apresentar o resultado. Cada operação deverá ser implementada em um método próprio. Divisões por zero
+e opções inexistentes deverão ser tratadas sem encerrar inesperadamente o programa.
+*/
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.Math;
