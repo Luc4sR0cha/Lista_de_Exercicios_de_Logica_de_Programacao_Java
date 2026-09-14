4c07a04a11720bd2e2d9cdf026003d887a5d5cfb
LucasRocha <lucasrochinha2007@gmail.com>
2026-09-14 14:46:05 -0300

    Resolvendo ex10 Main10.java

--- Exercício 10/Main10.java
+++ Exercício 10/Main10.java
@@ -17,8 +17,149 @@ Cada palavra poderá aparecer em qualquer uma das oito direções:
 Para cada palavra, o programa deverá informar se ela foi encontrada. Em caso positivo, deverá apresentar a posição inicial, a posição final e a direção em que foi localizada. As buscas não poderão ultrapassar os limites da matriz nem continuar do lado oposto ao atingir uma borda. Palavras poderão compartilhar letras. A verificação de uma palavra em determinada posição e direção deverá ser realizada por um método específico, reutilizado durante toda a busca.
 */
 
+import java.util.Scanner;
+
 public class Main10 {
+
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas: ");
+        int linhas = sc.nextInt();
+
+        System.out.print("Digite o número de colunas: ");
+        int colunas = sc.nextInt();
+
+        char[][] matriz = new char[linhas][colunas];
+
+        // Preencher matriz
+        System.out.println("\nDigite as letras:");
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print("[" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.next().toUpperCase().charAt(0);
+            }
+        }
+
+        System.out.print("\nQuantas palavras deseja procurar? ");
+        int quantidade = sc.nextInt();
+
+        String[] palavras = new String[quantidade];
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("Digite a palavra " + (i + 1) + ": ");
+            palavras[i] = sc.next().toUpperCase();
+        }
+
+        // Procurar palavras
+        for (int p = 0; p < palavras.length; p++) {
+
+            String palavra = palavras[p];
+            boolean encontrada = false;
+
+            for (int i = 0; i < linhas && !encontrada; i++) {
+
+                for (int j = 0; j < colunas && !encontrada; j++) {
+
+                    for (int direcao = 0; direcao < 8; direcao++) {
+
+                        int[] resultado = verificarPalavra(
+                                matriz, palavra, i, j, direcao
+                        );
+
+                        if (resultado != null) {
+
+                            System.out.println("\nPalavra: " + palavra);
+                            System.out.println(
+                                "Inicio: (" + i + ", " + j + ")"
+                            );
+                            System.out.println(
+                                "Fim: (" + resultado[0] + ", " + resultado[1] + ")"
+                            );
+                            System.out.println(
+                                "Direção: " + nomeDirecao(direcao)
+                            );
+
+                            encontrada = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!encontrada) {
+                System.out.println("\nPalavra: " + palavra);
+                System.out.println("Não encontrada.");
+            }
+        }
+
+        sc.close();
+    }
+
+    // Verifica palavra
+    public static int[] verificarPalavra(
+            char[][] matriz,
+            String palavra,
+            int linha,
+            int coluna,
+            int direcao) {
+
+        int[] linhaDirecao = {
+            0, 0, 1, -1, 1, -1, 1, -1
+        };
+
+        int[] colunaDirecao = {
+            1, -1, 0, 0, 1, -1, -1, 1
+        };
+
+        int linhaAtual = linha;
+        int colunaAtual = coluna;
+
+        for (int i = 0; i < palavra.length(); i++) {
+
+            if (linhaAtual < 0 ||
+                linhaAtual >= matriz.length ||
+                colunaAtual < 0 ||
+                colunaAtual >= matriz[0].length) {
+
+                return null;
+            }
+
+            if (matriz[linhaAtual][colunaAtual] != palavra.charAt(i)) {
+                return null;
+            }
+
+            linhaAtual += linhaDirecao[direcao];
+            colunaAtual += colunaDirecao[direcao];
+        }
+
+        return new int[] {
+            linhaAtual - linhaDirecao[direcao],
+            colunaAtual - colunaDirecao[direcao]
+        };
+    }
+
+    // Nome da direção
+    public static String nomeDirecao(int direcao) {
+
+        if (direcao == 0) {
+            return "Esquerda para direita";
+        } else if (direcao == 1) {
+            return "Direita para esquerda";
+        } else if (direcao == 2) {
+            return "Cima para baixo";
+        } else if (direcao == 3) {
+            return "Baixo para cima";
+        } else if (direcao == 4) {
+            return "Diagonal superior esquerda para inferior direita";
+        } else if (direcao == 5) {
+            return "Diagonal inferior direita para superior esquerda";
+        } else if (direcao == 6) {
+            return "Diagonal superior direita para inferior esquerda";
+        } else {
+            return "Diagonal inferior esquerda para superior direita";
+        }
     }
 }
--- Exercício 9/Main9.java
+++ Exercício 9/Main9.java
@@ -17,8 +17,239 @@ Na busca por assentos consecutivos, o usuário deverá informar a quantidade des
 */
 
 
+import java.util.Scanner;
+
 public class Main9 {
+
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean[][] assentos = new boolean[5][10];
+
+        int opcao;
+
+        do {
+            System.out.println("\n--- SISTEMA DE RESERVAS ---");
+            System.out.println("1 - Exibir mapa");
+            System.out.println("2 - Reservar assento");
+            System.out.println("3 - Cancelar reserva");
+            System.out.println("4 - Quantidade e percentual ocupados");
+            System.out.println("5 - Fileira com maior ocupação");
+            System.out.println("6 - Procurar assentos consecutivos");
+            System.out.println("0 - Sair");
+            System.out.print("Opção: ");
+            opcao = sc.nextInt();
+
+            if (opcao == 1) {
+                exibirMapa(assentos);
+
+            } else if (opcao == 2) {
+                reservar(assentos, sc);
+
+            } else if (opcao == 3) {
+                cancelar(assentos, sc);
+
+            } else if (opcao == 4) {
+                mostrarOcupacao(assentos);
+
+            } else if (opcao == 5) {
+                maiorOcupacao(assentos);
+
+            } else if (opcao == 6) {
+                procurarAssentos(assentos, sc);
+
+            } else if (opcao == 0) {
+                System.out.println("Programa encerrado!");
+
+            } else {
+                System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+
+        sc.close();
+    }
+
+    // Mapa
+    public static void exibirMapa(boolean[][] assentos) {
+
+        System.out.println("\n--- MAPA DE ASSENTOS ---");
+
+        for (int i = 0; i < assentos.length; i++) {
+
+            System.out.print("Fileira " + (i + 1) + ": ");
+
+            for (int j = 0; j < assentos[i].length; j++) {
+
+                if (assentos[i][j]) {
+                    System.out.print("[X] ");
+                } else {
+                    System.out.print("[ ] ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    // Reservar
+    public static void reservar(boolean[][] assentos, Scanner sc) {
+
+        System.out.print("Digite a fileira: ");
+        int fileira = sc.nextInt() - 1;
+
+        System.out.print("Digite o assento: ");
+        int assento = sc.nextInt() - 1;
+
+        if (fileira < 0 || fileira >= assentos.length ||
+            assento < 0 || assento >= assentos[0].length) {
+
+            System.out.println("Assento inválido!");
+
+        } else if (assentos[fileira][assento]) {
+
+            System.out.println("Esse assento já está ocupado!");
+
+        } else {
+
+            assentos[fileira][assento] = true;
+
+            System.out.println("Assento reservado!");
+        }
+    }
+
+    // Cancelar
+    public static void cancelar(boolean[][] assentos, Scanner sc) {
+
+        System.out.print("Digite a fileira: ");
+        int fileira = sc.nextInt() - 1;
+
+        System.out.print("Digite o assento: ");
+        int assento = sc.nextInt() - 1;
+
+        if (fileira < 0 || fileira >= assentos.length ||
+            assento < 0 || assento >= assentos[0].length) {
+
+            System.out.println("Assento inválido!");
+
+        } else if (!assentos[fileira][assento]) {
+
+            System.out.println("Esse assento está livre!");
+
+        } else {
+
+            assentos[fileira][assento] = false;
+
+            System.out.println("Reserva cancelada!");
+        }
+    }
+
+    // Ocupação
+    public static void mostrarOcupacao(boolean[][] assentos) {
+
+        int ocupados = 0;
+        int total = assentos.length * assentos[0].length;
+
+        for (int i = 0; i < assentos.length; i++) {
+
+            for (int j = 0; j < assentos[i].length; j++) {
+
+                if (assentos[i][j]) {
+                    ocupados++;
+                }
+            }
+        }
+
+        double percentual = (ocupados * 100.0) / total;
+
+        System.out.println("Assentos ocupados: " + ocupados);
+        System.out.println("Percentual ocupado: " + percentual + "%");
+    }
+
+    // Maior ocupação
+    public static void maiorOcupacao(boolean[][] assentos) {
+
+        int maior = 0;
+        int fileiraMaior = 0;
+
+        for (int i = 0; i < assentos.length; i++) {
+
+            int ocupados = 0;
+
+            for (int j = 0; j < assentos[i].length; j++) {
+
+                if (assentos[i][j]) {
+                    ocupados++;
+                }
+            }
+
+            if (ocupados > maior) {
+                maior = ocupados;
+                fileiraMaior = i;
+            }
+        }
+
+        System.out.println(
+            "Fileira com maior ocupação: " + (fileiraMaior + 1)
+        );
+
+        System.out.println("Assentos ocupados: " + maior);
+    }
+
+    // Buscar consecutivos
+    public static void procurarAssentos(
+            boolean[][] assentos,
+            Scanner sc) {
+
+        System.out.print("Quantos assentos deseja? ");
+        int quantidade = sc.nextInt();
+
+        if (quantidade <= 0 || quantidade > assentos[0].length) {
+            System.out.println("Quantidade inválida!");
+            return;
+        }
+
+        boolean encontrou = false;
+
+        for (int i = 0; i < assentos.length; i++) {
+
+            int contador = 0;
+
+            for (int j = 0; j < assentos[i].length; j++) {
+
+                if (!assentos[i][j]) {
+                    contador++;
+                } else {
+                    contador = 0;
+                }
+
+                if (contador == quantidade) {
+
+                    int inicio = j - quantidade + 1;
+
+                    System.out.println(
+                        "Encontrado na fileira " + (i + 1)
+                    );
+
+                    System.out.print("Assentos: ");
+
+                    for (int k = inicio; k <= j; k++) {
+                        System.out.print((k + 1) + " ");
+                    }
+
+                    System.out.println();
+
+                    encontrou = true;
+                    return;
+                }
+            }
+        }
+
+        if (!encontrou) {
+            System.out.println(
+                "Não existe essa quantidade de assentos consecutivos."
+            );
+        }
     }
 }
